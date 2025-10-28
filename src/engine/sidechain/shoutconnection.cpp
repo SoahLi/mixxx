99b321f6f91d6b395df3bc67f5e06c38f7a02b4b
Owen Turnbull <ows8turnbull@gmail.com>
2025-10-28 16:22:42 -0700

    feat: Additional Metadata Variables for Broadcasting

--- src/engine/sidechain/shoutconnection.cpp
+++ src/engine/sidechain/shoutconnection.cpp
@@ -35,8 +35,17 @@ constexpr int kMaxNetworkCache = 491520; // 10 s mp3 @ 192 kbit/s
 // http://wiki.shoutcast.com/wiki/SHOUTcast_DNAS_Server_2
 constexpr int kMaxShoutFailures = 3;
 
-const QRegularExpression kArtistOrTitleRegex(QStringLiteral("\\$artist|\\$title"));
-const QRegularExpression kArtistRegex(QStringLiteral("\\$artist"));
+const QRegularExpression kMetaDataRegex(
+        QStringLiteral("\\$artist|\\$title|\\$album|\\$year|\\$genre|\\$bpm"));
+const QRegularExpression kMetaDataKeys[] = {
+        QRegularExpression(QStringLiteral("\\$artist")),
+        QRegularExpression(QStringLiteral("\\$title")),
+        QRegularExpression(QStringLiteral("\\$album")),
+        QRegularExpression(QStringLiteral("\\$year")),
+        QRegularExpression(QStringLiteral("\\$genre")),
+        QRegularExpression(QStringLiteral("\\$bpm")),
+};
+const int kMetaDataKeyCount = 6;
 
 const mixxx::Logger kLogger("ShoutConnection");
 
@@ -816,9 +825,14 @@ void ShoutConnection::updateMetaData() {
     // metadata being enabled, we want dynamic metadata changes
     if (!m_custom_metadata && (m_format_is_mp3 || m_format_is_aac || m_ogg_dynamic_update)) {
         if (m_pMetaData != nullptr) {
-
             QString artist = m_pMetaData->getArtist();
-            QString title = m_pMetaData->getTitle();
+            const QString metaDataValues[] = {
+                    artist,
+                    m_pMetaData->getTitle(),
+                    m_pMetaData->getAlbum(),
+                    m_pMetaData->getYear(),
+                    m_pMetaData->getGenre(),
+                    QString::number(m_pMetaData->getBpm())};
 
             // shoutcast uses only "song" as field for "artist - title".
             // icecast2 supports separate fields for "artist" and "title",
@@ -831,41 +845,43 @@ void ShoutConnection::updateMetaData() {
             // Also I do not know about icecast1. To be safe, i stick to the
             // old way for those use cases.
             if (!m_format_is_mp3 && m_protocol_is_icecast2) {
-            	setFunctionCode(9);
-            	insertMetaData("artist", encodeString(artist).constData());
-            	insertMetaData("title", encodeString(title).constData());
-            } else {
-                // we are going to take the metadata format and replace all
-                // the references to $title and $artist by doing a single
-                // pass over the string
-                int replaceIndex = 0;
+                setFunctionCode(9);
+                insertMetaData("artist", encodeString(artist).constData());
+            }
 
-                // Make a copy so we don't overwrite the references only
-                // once per streaming session.
-                QString metadataFinal = m_metadataFormat;
-                do {
-                    // find the next occurrence
-                    replaceIndex = metadataFinal.indexOf(
-                            kArtistOrTitleRegex,
-                            replaceIndex);
+            // we are going to take the metadata format and replace all
+            // the references to $title and $artist by doing a single
+            // pass over the string
+            int replaceIndex = 0;
 
-                    if (replaceIndex != -1) {
+            // Make a copy so we don't overwrite the references only
+            // once per streaming session.
+            QString metadataFinal = m_metadataFormat;
+            do {
+                replaceIndex = metadataFinal.indexOf(
+                        kMetaDataRegex,
+                        replaceIndex);
+
+                if (replaceIndex != -1) {
+                    for (int i = 0; i < kMetaDataKeyCount; ++i) {
+                        QRegularExpression key = kMetaDataKeys[i];
+                        QString value = metaDataValues[i];
                         if (metadataFinal.indexOf(
-                                    kArtistRegex, replaceIndex) == replaceIndex) {
-                            metadataFinal.replace(replaceIndex, 7, artist);
-                            // skip to the end of the replacement
-                            replaceIndex += artist.length();
-                        } else {
-                            metadataFinal.replace(replaceIndex, 6, title);
-                            replaceIndex += title.length();
+                                    key, replaceIndex) == replaceIndex) {
+                            metadataFinal.replace(
+                                    replaceIndex,
+                                    key.pattern().replace("\\", "").length(),
+                                    value);
+                            replaceIndex += value.length();
+                            break;
                         }
                     }
-                } while (replaceIndex != -1);
+                }
+            } while (replaceIndex != -1);
 
-                QByteArray baSong = encodeString(metadataFinal);
-                setFunctionCode(10);
-                insertMetaData("song",  baSong.constData());
-            }
+            QByteArray baSong = encodeString(metadataFinal);
+            setFunctionCode(10);
+            insertMetaData("song", baSong.constData());
             setFunctionCode(11);
             int ret = shout_set_metadata(m_pShout, m_pShoutMetaData);
             if (ret != SHOUTERR_SUCCESS) {
